5695ac19b068f4ac818d4d41e1723780fbf019e0
unknown <angelopedrosa00@gmail.com>
2023-07-12 08:20:07 -0300

    Pagamento por cartão

--- src/pages/checkout/index.tsx
+++ src/pages/checkout/index.tsx
@@ -1,54 +1,116 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Card from '../../components/Card'
-import { InpuitGroup, Row } from './styles'
+import { InpuitGroup, Row, TabButton } from './styles'
 import Button from '../../components/Button'
+import boleto from "../../asserts/images/barcode.png"
+import cartao from "../../asserts/images/credit-card.png"
 
-const Checkout = () => (
-  <div className="container">
-    <Card title="Dados de cobrança">
+const Checkout = () =>{
+  const [payWithCar, setPayWithCar] = useState(false)
+  return (
+    <div className="container">
+      <Card title="Dados de cobrança">
+        <>
+          <Row>
+            <InpuitGroup>
+              <label htmlFor="fullName">Nome completo</label>
+              <input type="text" id="fullName" />
+            </InpuitGroup>
+            <InpuitGroup>
+              <label htmlFor="email">E-mail</label>
+              <input type="text" id="email" />
+            </InpuitGroup>
+            <InpuitGroup>
+              <label htmlFor="cpf">CPF</label>
+              <input type="text" id="cpf" />
+            </InpuitGroup>
+          </Row>
+          <h3 className="margin-top">Dados de entrega - conteúdo digital</h3>
+          <Row>
+            <InpuitGroup maxWidth='284px'>
+              <label htmlFor="deliveryEmail">E-mail</label>
+              <input type="text" id="deliveryEmail" />
+            </InpuitGroup>
+            <InpuitGroup maxWidth='284px'>
+              <label htmlFor="confirmDeliveryEmail">Confirme o e-mail</label>
+              <input type="text" id="confirmDeliveryEmail" />
+            </InpuitGroup>
+          </Row>
+        </>
+      </Card>
+      <Card title="Pagamento">
       <>
-        <Row>
-          <InpuitGroup>
-            <label htmlFor="fullName">Nome completo</label>
-            <input type="text" id="fullName" />
-          </InpuitGroup>
-          <InpuitGroup>
-            <label htmlFor="email">E-mail</label>
-            <input type="text" id="email" />
-          </InpuitGroup>
-          <InpuitGroup>
-            <label htmlFor="cpf">CPF</label>
-            <input type="text" id="cpf" />
-          </InpuitGroup>
-        </Row>
-        <h3 className="margin-top">Dados de entrega - conteúdo digital</h3>
-        <Row>
-          <InpuitGroup>
-            <label htmlFor="deliveryEmail">E-mail</label>
-            <input type="text" id="deliveryEmail" />
-          </InpuitGroup>
-          <InpuitGroup>
-            <label htmlFor="confirmDeliveryEmail">Confirme o e-mail</label>
-            <input type="text" id="confirmDeliveryEmail" />
-          </InpuitGroup>
-        </Row>
+        <TabButton isActive ={payWithCar} onClick={() => setPayWithCar(true)}>
+          <img src={boleto} alt="Boleto" />
+          Boleto bancário
+        </TabButton>
+        <TabButton isActive ={!payWithCar} onClick={() => setPayWithCar(false)}>
+          <img src={cartao} alt="Cartão" />
+          Cartão de crédito
+        </TabButton>
+        <div className='margin-top'>
+          {payWithCar ? (
+              <p>
+                Ao optar por essa forma de pagamento, é importante lembrar que a
+                onfirmação pode levar até 3 dias úteis, devido aos prazos
+                estabelecidos pelas instituições financeiras. Portanto, a liberação do
+                código de ativação do jogo adquirido ocorrerá somente após a aprovação
+                do pagamento do boleto.
+              </p>
+            ) : (
+            <>
+            <Row >
+                <InpuitGroup>
+                  <label htmlFor="cardOnew">Nome do titular do cartão</label>
+                  <input type="text" id='cardOnew' />
+                </InpuitGroup>
+                <InpuitGroup>
+                  <label htmlFor="cpfCardOnew">CPF do titular do cartão</label>
+                  <input type="text" id='cpfCardOnew' />
+                </InpuitGroup>
+            </Row>
+            <Row marginTop='24px'>
+                <InpuitGroup>
+                  <label htmlFor="cardDisplayName">Nome no cartão</label>
+                  <input type="text" id='cardDisplayName' />
+                </InpuitGroup>
+                <InpuitGroup>
+                  <label htmlFor="cardNumber">Número do cartão</label>
+                  <input type="text" id='cardNumber' />
+                </InpuitGroup>
+                <InpuitGroup maxWidth="123px">
+                  <label htmlFor="expiresMoth">Mês do vencimento</label>
+                  <input type="text" id='expiresMoth' />
+                </InpuitGroup>
+                <InpuitGroup maxWidth="123px">
+                  <label htmlFor="expiresYear">Ano de vencimento</label>
+                  <input type="text" id='expiresYear' />
+                </InpuitGroup>
+                <InpuitGroup maxWidth="48px">
+                  <label htmlFor="cardCode">CVV</label>
+                  <input type="text" id='cardCode' />
+                </InpuitGroup>
+            </Row>
+            <Row marginTop='24px'>
+              <InpuitGroup maxWidth="150px">
+                <label htmlFor="installments">Parcelamento</label>
+                <select>
+                  <option>1x de R$ 200,00</option>
+                  <option>2x de R$ 100,00</option>
+                  <option>3x de R$ 66,66</option>
+                </select>
+              </InpuitGroup>
+            </Row>
+          </>)}
+        </div>
       </>
-    </Card>
-    <Card title="Pagamento">
-      <>
-        <p>
-          Ao optar por essa forma de pagamento, é importante lembrar que a
-          confirmação pode levar até 3 dias úteis, devido aos prazos
-          estabelecidos pelas instituições financeiras. Portanto, a liberação do
-          código de ativação do jogo adquirido ocorrerá somente após a aprovação
-          do pagamento do boleto.
-        </p>
-      </>
-    </Card>
-    <Button type="button" title="Cliquei aqui para finaliza a compra">
-      Finalizar compra
-    </Button>
-  </div>
-)
+      </Card>
+
+      <Button type="button" title="Cliquei aqui para finaliza a compra">
+        Finalizar compra
+      </Button>
+    </div>
+  )
+}
 
 export default Checkout
--- src/pages/checkout/styles.ts
+++ src/pages/checkout/styles.ts
@@ -1,24 +1,53 @@
 import styled from 'styled-components'
 import { cores } from '../../styles'
 
-export const Row = styled.div`
+type InpuitGroupProps = {
+  maxWidth?: string
+}
+type RowProps = {
+  marginTop?: string
+}
+type TagProps = {
+  isActive: boolean
+}
+
+export const Row = styled.div<RowProps>`
   display: flex;
   column-gap: 24px;
+  margin-top: ${(props) => props.marginTop || '0'};
+  align-items: flex-end;
 `
 
-export const InpuitGroup = styled.div`
+export const InpuitGroup = styled.div<InpuitGroupProps>`
   flex: auto;
+  max-width: ${(props) => props.maxWidth || 'auto'};
   label {
     font-size: 14px;
     margin-bottom: 8px;
     display: block;
   }
 
-  input {
+  input, select{
     background-color: ${cores.branca};
     height: 32px;
     width: 100%;
     padding: 0px 8px;
     border: 1px solid ${cores.branca};
+  }
+`
+
+export const TabButton = styled.button<TagProps>`
+  border-radius: 8px;
+  font-size: 14px;
+  font-weight: bold;
+  color: ${cores.branca};
+  background-color: ${props => props.isActive ? cores.verde : cores.preta};
+  border: none;
+  height: 32px;
+  margin-right: 16px;
+  padding: 0px 8px;
+  cursor: pointer;
+  img {
+    margin-right: 8px;
   }
 `
